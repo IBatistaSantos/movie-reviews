d85f026cc46972b64c216f737c64f4290edb34a6
Israel Batista <israelbatiista19@gmail.com>
2024-10-07 20:17:52 -0300

    feat: add jwt module in auth module

---
+++ src/modules/auth/auth.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthenticationService } from './services/auth.service';
+import { BcryptProvider } from '../users/infrastructure/providers/bcrypt.provider';
+import { UserRepositoryImpl } from '../users/infrastructure/repository/user-typeorm.repository';
+import { JWTProviderImpl } from '../shared/jwt/jwt-impl.provider';
+import { JwtStrategy } from './services/jwt.strategy';
+import { AuthController } from './controllers/auth.controller';
+import { UserSchema } from '../users/infrastructure/repository/schemas/user.schema';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([UserSchema]),
+  ],
+  providers: [
+    AuthenticationService,
+    { provide: 'HashProvider', useClass: BcryptProvider },
+    { provide: 'UserRepository', useClass: UserRepositoryImpl },
+    { provide: 'JWTProvider', useClass: JWTProviderImpl },
+    JwtStrategy,
+  ],
+  controllers: [AuthController],
+  exports: [],
+})
+export class AuthenticationModule {}
