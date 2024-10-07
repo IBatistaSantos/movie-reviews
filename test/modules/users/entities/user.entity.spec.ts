24616d29464fec25f97ab144b2c6dac1f3b232a1
Israel Batista <israelbatiista19@gmail.com>
2024-10-07 17:12:48 -0300

    test: create unit test for user entity

---
+++ test/modules/users/entities/user.entity.spec.ts
@@ -0,0 +1,32 @@
+import { faker } from '@faker-js/faker';
+import { User } from '../../../../src/modules/users/entities/user.entity';
+
+describe('UserEntity', () => {
+  it('should create a new UserEntity', () => {
+    const userEntity = new User({
+      email: faker.internet.email(),
+      name: faker.person.fullName(),
+      password: faker.internet.password(),
+    });
+    expect(userEntity).toBeInstanceOf(User);
+    expect(userEntity.id).toBeDefined();
+  });
+
+  it('should return the JSON representation of the UserEntity', () => {
+    const userEntity = new User({
+      email: faker.internet.email(),
+      name: faker.person.fullName(),
+      password: faker.internet.password(),
+    });
+    const json = userEntity.toJSON();
+    expect(json).toEqual({
+      id: userEntity.id,
+      createdAt: userEntity.createdAt,
+      updatedAt: userEntity.updatedAt,
+      status: userEntity.status,
+      name: userEntity.name,
+      email: userEntity.email.value,
+      password: userEntity.password,
+    });
+  });
+});
