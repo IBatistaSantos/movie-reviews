4d9207ae772750621813ea13e7eca0f66a8f162c
Israel Batista <israelbatiista19@gmail.com>
2024-10-07 17:36:11 -0300

    feat: create service for creation users

---
+++ src/modules/users/services/create-user.service.ts
@@ -0,0 +1,37 @@
+import { Inject, Injectable } from '@nestjs/common';
+
+import { User } from '../entities/user.entity';
+import { BaseError } from '../../../../src/core/errors/base-error';
+
+import { HashProvider } from '../providers/hash.provider';
+import { UserRepository } from '../repositories/user.repository';
+
+@Injectable()
+export class CreateUserService {
+  constructor(
+    @Inject('UserRepository')
+    private readonly repository: UserRepository,
+    @Inject('HashProvider')
+    private readonly hashProvider: HashProvider,
+  ) {}
+
+  async execute(name: string, email: string, password: string) {
+    const userAlreadyExists = await this.repository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new BaseError('User already exists');
+    }
+
+    const passwordHash = await this.hashProvider.hash(password);
+
+    const user = new User({
+      email,
+      name,
+      password: passwordHash,
+    });
+
+    await this.repository.save(user);
+
+    return { id: user.id };
+  }
+}
