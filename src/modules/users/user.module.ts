6d3dd0fc28c458ab68172af91e5e12ca382b9a17
Israel Batista <israelbatiista19@gmail.com>
2024-10-07 20:18:26 -0300

    feat: add providers and controller in user module

--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSchema } from './infrastructure/repository/schemas/user.schema';
+import { CreateUserService } from './services/create-user.service';
+import { BcryptProvider } from './infrastructure/providers/bcrypt.provider';
+import { UserController } from './controllers/user.controller';
+import { UserRepositoryImpl } from './infrastructure/repository/user-typeorm.repository';
+import { ProfileUserService } from './services/profile-user.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([UserSchema])],
+  providers: [
+    CreateUserService,
+    ProfileUserService,
+    {
+      provide: 'UserRepository',
+      useClass: UserRepositoryImpl,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptProvider,
+    },
+  ],
+  controllers: [UserController],
+  exports: ['UserRepository'],
 })
 export class UserModule {}
