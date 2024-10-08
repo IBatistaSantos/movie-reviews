dd23bda33b7ccbd940ef0a591dde7e3d9b9a2c33
Israel Batista <israelbatiista19@gmail.com>
2024-10-08 16:46:38 -0300

    feat: create movie review service

---
+++ src/modules/movie-reviews/services/create-movie-reviews.service.ts
@@ -0,0 +1,59 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { MovieRepository } from '../repositories/movie.repository';
+import { MovieProvider, SearchMovieProvider } from '../providers/search-movie';
+import { Movie } from '../entity/movie';
+import { MovieNotFound } from '../entity/errors/movie-not-found';
+
+interface Input {
+  userId: string;
+  title: string;
+  notes: string;
+}
+
+@Injectable()
+export class CreateMovieReviewService {
+  constructor(
+    @Inject('MovieRepository')
+    private readonly repository: MovieRepository,
+
+    @Inject('SearchMovieProvider')
+    private readonly searchMovieProvider: SearchMovieProvider,
+  ) {}
+
+  async execute(params: Input) {
+    const { userId, title, notes } = params;
+
+    const movieInformation = await this.searchMovieProvider.search(title);
+
+    if (!movieInformation) {
+      throw new MovieNotFound();
+    }
+
+    let movie = await this.repository.findByTitle(movieInformation.title);
+    if (!movie) {
+      movie = await this.createMovie(movieInformation);
+    }
+
+    await this.repository.createReview({
+      movieId: movie.id,
+      userId,
+      notes,
+    });
+  }
+
+  private async createMovie(movie: MovieProvider) {
+    const newMovie = new Movie({
+      title: movie.title,
+      actors: movie.actors,
+      genre: movie.genre,
+      poster: movie.poster,
+      rating: movie.rating,
+      released: movie.released,
+      year: movie.year,
+    });
+
+    await this.repository.save(newMovie);
+
+    return newMovie;
+  }
+}
