db0f3ada3f7e0f340b794882b01e86a602193c39
Israel Batista <israelbatiista19@gmail.com>
2024-10-07 22:12:15 -0300

    feat: create method in controller

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -2,12 +2,17 @@ import { Body, Controller, Post } from '@nestjs/common';
 
 import { SignInDto } from './dtos/signIn.dto';
 import { AuthenticationService } from '../services/auth.service';
+import { ForgotPasswordService } from '../services/forgot-password.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ForgotPasswordDTO } from './dtos/forgotPassword.dto';
 
 @Controller('auth')
 @ApiTags('Authentication')
 export class AuthController {
-  constructor(private readonly authService: AuthenticationService) {}
+  constructor(
+    private readonly authService: AuthenticationService,
+    private readonly forgotPasswordService: ForgotPasswordService,
+  ) {}
 
   @Post()
   @ApiResponse({
@@ -30,5 +35,14 @@ export class AuthController {
   })
   async signIn(@Body() authenticationDto: SignInDto) {
     return await this.authService.execute(authenticationDto);
+  }
+
+  @Post('forgot-password')
+  @ApiResponse({
+    status: 200,
+    description: 'The user has been successfully authenticated.',
+  })
+  async forgotPassword(@Body() params: ForgotPasswordDTO) {
+    return await this.forgotPasswordService.execute(params);
   }
 }
