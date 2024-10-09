b168b0719f4636c3a175d26aa1ca7dd5302297a0
Israel Batista <israelbatiista19@gmail.com>
2024-10-09 10:14:35 -0300

    feat: create method list movie review in controller

--- src/modules/movie-reviews/controllers/movie-review.controller.ts
+++ src/modules/movie-reviews/controllers/movie-review.controller.ts
@@ -6,9 +6,10 @@ import {
   Param,
   Post,
   Put,
+  Query,
   UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { CreateMovieReviewService } from '../services/create-movie-reviews.service';
 import { JwtAuthGuard } from '@/modules/auth/guard/jwt-auth.guard';
@@ -18,6 +19,10 @@ import { DetailMovieReviewService } from '../services/details-movie-review.servi
 import { DeleteMovieReviewsService } from '../services/delete-movie-reviews.service';
 import { UpdateMovieReviewService } from '../services/update-movie-review.service';
 import { UpdateMovieReviewDTO } from './dtos/update-review.dto';
+import {
+  ListMovieReviewsOutput,
+  ListMovieReviewsService,
+} from '../services/list-movie-reviews.service';
 
 @UseGuards(JwtAuthGuard)
 @ApiTags('movie-reviews')
@@ -26,6 +31,7 @@ export class MovieReviewController {
   constructor(
     private readonly createMovieService: CreateMovieReviewService,
     private readonly detailMovieReviewService: DetailMovieReviewService,
+    private readonly listMovieReviewService: ListMovieReviewsService,
     private readonly deleteMovieReviewService: DeleteMovieReviewsService,
     private readonly updateMovieReviewService: UpdateMovieReviewService,
   ) {}
@@ -75,6 +81,81 @@ export class MovieReviewController {
       reviewId,
       userId: user.id,
       notes: data.notes,
+    });
+  }
+
+  @Get()
+  @ApiResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        page: { type: 'number' },
+        limit: { type: 'number' },
+        hasMore: { type: 'boolean' },
+        data: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              notes: { type: 'string' },
+              userId: { type: 'string' },
+              name: { type: 'string' },
+              movie: {
+                type: 'object',
+                properties: {
+                  id: { type: 'string' },
+                  title: { type: 'string' },
+                  actor: { type: 'string' },
+                  poster: { type: 'string' },
+                  rating: { type: 'number' },
+                  genre: { type: 'string' },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  })
+  @ApiQuery({ name: 'title', required: false })
+  @ApiQuery({ name: 'actor', required: false })
+  @ApiQuery({ name: 'director', required: false })
+  @ApiQuery({ name: 'sortBy', required: false })
+  @ApiQuery({ name: 'order', required: false, enum: ['ASC', 'DESC'] })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    schema: { default: 10 },
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    schema: { default: 1 },
+  })
+  async listMovieReviews(
+    @GetUser() user: any,
+    @Query('title') title?: string,
+    @Query('actor') actor?: string,
+    @Query('director') director?: string,
+    @Query('sortBy') sortBy?: string,
+    @Query('order') order?: 'ASC' | 'DESC',
+    @Query('limit') limit?: number,
+    @Query('page') page?: number,
+  ): Promise<ListMovieReviewsOutput> {
+    const userId = user.id;
+    return this.listMovieReviewService.execute({
+      userId,
+      options: {
+        limit,
+        page,
+        title,
+        actor,
+        director,
+        sortBy,
+        order,
+      },
     });
   }
 }
